b653086d293727dd49911d9ce058bcb86d89e44f
ram.sinha <ram.sinha@guavus.com>
2014-12-20 21:31:22 +0530

    Generalized the sorting algo

--- python_codes/sorting/mergeSort.py
+++ python_codes/sorting/mergeSort.py
@@ -15,22 +15,13 @@ def merge(array, p, q, r):
             array[k] = secondPart[j]
             j+=1
 
-l = [1,24,46, 2,3,4,3,56,7,8,9,99,777,34232]
-#merge(l,0,2,5)
-#print l
 def mergeSort(array,p,r):
     if p < r:
         q = ( p + r )/2 
         mergeSort(array, p, q)
         mergeSort(array, q+1, r)
         merge(array, p, q, r)
-mergeSort(l,0,len(l)-1)
-print l
-"""
 if __name__=='__main__':
-    params = sys.argv
-    if len(params) < 2:
-        print "Please enter the array to be sorted"
-        sys.exit(0)
-    mergeSort([for int(i) in raw_input().split(' ')])
-"""
+    input = [int(i) for i in  raw_input().split(' ')]
+    mergeSort(input,0, len(input)-1)
+    print input
