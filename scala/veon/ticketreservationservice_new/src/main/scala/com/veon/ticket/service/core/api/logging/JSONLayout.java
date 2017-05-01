083020b6ab869be0800615c46615c3ec6cd5ae4d
ram.sinha <ramsinha@adobe.com>
2017-05-01 14:25:58 +0530

    Removing comp details

--- scala/veon/ticketreservationservice_new/src/main/scala/com/veon/ticket/service/core/api/logging/JSONLayout.java
+++ scala/veon/ticketreservationservice_new/src/main/scala/com/veon/ticket/service/core/api/logging/JSONLayout.java
@@ -1,23 +1,4 @@
 package com.veon.ticket.service.core.api.logging;
-/*
- * ************************************************************************
- *  ADOBE CONFIDENTIAL
- *  ___________________
- *
- *   Copyright 2014 Adobe Systems Incorporated
- *   All Rights Reserved.
- *
- *  NOTICE:  All information contained herein is, and remains
- *  the property of Adobe Systems Incorporated and its suppliers,
- *  if any.  The intellectual and technical concepts contained
- *  herein are proprietary to Adobe Systems Incorporated and its
- *  suppliers and are protected by all applicable intellectual property
- *  laws, including trade secret and copyright laws.
- *  Dissemination of this information or reproduction of this material
- *  is strictly forbidden unless prior written permission is obtained
- *  from Adobe Systems Incorporated.
- * ************************************************************************
- */
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
