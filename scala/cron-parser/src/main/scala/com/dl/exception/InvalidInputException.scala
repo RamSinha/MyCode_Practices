470af4d7ef1b552f3ae8cf4f0c19295e663b9697
ram.sinha <ram.sinha@careem.com>
2022-01-25 21:32:42 +0400

    Removing cronparse

--- scala/cron-parser/src/main/scala/com/dl/exception/InvalidInputException.scala
+++
@@ -1,6 +0,0 @@
-package com.dl.exception
-
-/**
- * @author ram.sinha on 1/24/22
- */
-case class InvalidInputException(msg : String) extends Exception(msg)
--- scala/cron-parser/src/main/scala/com/dl/exception/OutOfRangeException.scala
+++
@@ -1,6 +0,0 @@
-package com.dl.exception
-
-/**
- * @author ram.sinha on 1/24/22
- */
-case class OutOfRangeException(msg : String) extends Exception(msg)
